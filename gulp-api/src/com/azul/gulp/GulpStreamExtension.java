a54bc78fa75a3067830319b946eb40a1b7ee9960
Douglas Hawkins <dougqh@gmail.com>
2018-10-18 14:54:40 +0300

    Restoring extension overrides that narrow return type that were erroneously removed during clean-up

--- gulp-api/src/com/azul/gulp/GulpStreamExtension.java
+++ gulp-api/src/com/azul/gulp/GulpStreamExtension.java
@@ -3,4 +3,22 @@ package com.azul.gulp;
 public interface GulpStreamExtension<E extends GulpStreamExtension<E, T>, T> 
   extends GulpStream<T>
 {
+  public abstract E filter(Predicate<? super T> predicate);
+  
+  public abstract E filter(Set<? super T> set);
+  
+  @SuppressWarnings("unchecked")
+  public abstract E filter(T... set);
+  
+  public abstract E filterOut(Predicate<? super T> predicate);
+  
+  public abstract E filterOut(Set<? super T> set);
+  
+  @SuppressWarnings("unchecked")
+  public abstract E filterOut(T... set);
+  
+  public abstract E unique();
+  
+  public abstract E unique(final ThrowingFunction<? super T, ?> identityFn);
+
 }
